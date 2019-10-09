875deb224575f7c3c6fd61ad99bc5d6aabc61792
Sahil Singh <sahil.dogstar@gmail.com>
2019-10-09 23:10:00 +0530

    Fit and Finish Done.

--- lib/main.dart
+++ lib/main.dart
@@ -3,44 +3,41 @@ import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      //appBar: AppBar(
-        //title: Text('Strawberry Pavlova'),
-      //),
-      body: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.red, width: 2),
-        ),
-        child: Row(
+    home: food()
+      ),
+
+
+  );
+}
+
+class food extends StatefulWidget {
+  @override
+  _foodState createState() => _foodState();
+}
+
+class _foodState extends State<food> {
+    Widget _layOut(){
+      Orientation orientation = MediaQuery.of(context).orientation;
+      if(orientation == Orientation.landscape){
+        return Row(
           children: <Widget>[
             Expanded(
               flex: 1,
               child: Column(
                 children: <Widget>[
                   Container(
-                    margin: EdgeInsets.fromLTRB(8.0, 24.0, 10.0, 2.0),
+                    margin: EdgeInsets.fromLTRB(8.0, 40.0, 10.0, 2.0),
                     padding: EdgeInsets.fromLTRB(4.0, 2.0, 4.0, 8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade100,
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                    ),
+
                     child: Center(
                       child: Text(
-                        "Strawberry pavlova",
+                        "Strawberry Pavolva", textAlign: TextAlign.center,
                         style: TextStyle(
                             fontSize: 18, fontWeight: FontWeight.bold),
                       ),
                     ),
                   ),
                   Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade100,
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                    ),
                     margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
                     child: Center(
                       child: Text(
@@ -56,66 +53,59 @@ void main() {
                   ),
                   Container(
                     margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade100,
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    child: Wrap(
+                      spacing: 8,
+                      runSpacing: 4,
+                      alignment: WrapAlignment.center,
+                      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Icon(
                               Icons.star,
-                              size: 12,
+                              size: 24,
                               color: Colors.grey,
                             ),
                             Icon(
                               Icons.star,
-                              size: 12,
+                              size: 24,
                               color: Colors.grey,
                             ),
                             Icon(
                               Icons.star,
-                              size: 12,
+                              size: 24,
                               color: Colors.grey,
                             ),
                             Icon(
                               Icons.star,
-                              size: 12,
+                              size: 24,
                               color: Colors.grey,
                             ),
                             Icon(
                               Icons.star,
-                              size: 12,
+                              size: 24,
                               color: Colors.grey,
                             ),
+                            Container(
+                              child: Text('170 Reviews'),
+                            )
                           ],
                         ),
-                        Container(
-                          child: Text('170 Reviews'),
-                        )
+
                       ],
                     ),
                   ),
                   Container(
                     margin:
                     EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 12.0),
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade100,
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
                         Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Icon(Icons.thumb_up),
+                            Icon(Icons.check_circle, color: Colors.green,),
                             Text('PREP'),
                             Text('25 Min'),
                           ],
@@ -123,7 +113,7 @@ void main() {
                         Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Icon(Icons.timer),
+                            Icon(Icons.timer, color: Colors.green,),
                             Text('COOK'),
                             Text('1 Hr'),
                           ],
@@ -131,7 +121,7 @@ void main() {
                         Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Icon(Icons.fastfood),
+                            Icon(Icons.perm_identity, color: Colors.green,),
                             Text('FEEDS'),
                             Text('4-6'),
                           ],
@@ -147,6 +137,7 @@ void main() {
               child:
               Container(
                 child: OverflowBox(
+                  alignment: Alignment(-1, 0),
                   minHeight: 0.0,
                   minWidth: 0.0,
                   maxWidth: double.infinity,
@@ -156,12 +147,166 @@ void main() {
               ),
             ),
           ],
-        ),
-      ),
+        );
+      }
 
-    ),
-      ),
+      else if(orientation == Orientation.portrait){
+        return Column(
+          children: <Widget>[
+            Expanded (
+              flex: 1,
+              child:
+              Container(
+                child: OverflowBox(
+                  //alignment: Alignment(-1, 0),
+                  //minHeight: 0.0,
+                  //minWidth: 0.0,
+                  //maxWidth: double.infinity,
+                  child: Image
+                    (image: AssetImage('assets/food.png'), fit: BoxFit.cover,),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 24, 0, 0),
+                        child: Center(
+                          child: Text(
+                            "Strawberry Pavolva",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold, ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+                    child: Center(
+                      child: Text(
+                        "pavlova is a meringue-based"
+                            " dessert names after the Russian"
+                            " ballerine Arina Pavlova."
+                            "Pavlova features a crisp crust and "
+                            "soft, light inside, topped with fruit"
+                            "and whipped cream. ",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+                    child: Wrap(
+                      spacing: 8,
+                      runSpacing: 4,
+                      alignment: WrapAlignment.center,
+                      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.star,
+                                size: 24,
+                                color: Colors.grey,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 24,
+                                color: Colors.grey,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 24,
+                                color: Colors.grey,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 24,
+                                color: Colors.grey,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 24,
+                                color: Colors.grey,
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                                child: Text('170 Reviews'),
+                              )
+                            ],
+                          ),
+                        ),
 
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin:
+                    EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Icon(Icons.check_circle, color: Colors.green,),
+                            Text('PREP'),
+                            Text(""),
+                            Text('25 Min'),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Icon(Icons.timer, color: Colors.green,),
+                            Text('COOK'),
+                            Text(""),
+                            Text('1 Hr'),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Icon(Icons.perm_identity, color: Colors.green,),
+                            Text('FEEDS'),
+                            Text(""),
+                            Text('4-6'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        );
+      }
+    }
+      //body: Container(
+        //decoration: BoxDecoration(
+          //border: Border.all(color: Colors.red, width: 2),
+        //),
 
-  );
+      //),
+
+      @override
+      Widget build(BuildContext context) {
+    return Scaffold(
+      body: _layOut());
+
+  }
 }
+
+
+

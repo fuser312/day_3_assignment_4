58ab5fbabfc1bce378bd0a51abbeea045e0156a9
Sahil Singh <sahil.dogstar@gmail.com>
2019-10-13 21:08:41 +0530

    Hopefully Fit and Finish is done. Brought code line numbers from over 300 to less than 200.

--- lib/main.dart
+++ lib/main.dart
@@ -1,312 +1,188 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: food()
-      ),
-
-
-  );
+    home: Scaffold(
+      body: Food(),
+    ),
+  ));
 }
 
-class food extends StatefulWidget {
+class Food extends StatelessWidget {
   @override
-  _foodState createState() => _foodState();
-}
-
-class _foodState extends State<food> {
-    Widget _layOut(){
-      Orientation orientation = MediaQuery.of(context).orientation;
-      if(orientation == Orientation.landscape){
-        return Row(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.fromLTRB(8.0, 40.0, 10.0, 2.0),
-                    padding: EdgeInsets.fromLTRB(4.0, 2.0, 4.0, 8.0),
-
-                    child: Center(
-                      child: Text(
-                        "Strawberry Pavolva", textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-                    child: Center(
-                      child: Text(
-                        "pavlova is a meringue-based"
-                            " dessert names after the Russian"
-                            " ballerine Arina Pavlova."
-                            "Pavlova features a crisp crust and "
-                            "soft, light inside, topped with fruit"
-                            "and whipped cream. ",
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-                    child: Wrap(
-                      spacing: 8,
-                      runSpacing: 4,
-                      alignment: WrapAlignment.center,
-                      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              Icons.star,
-                              size: 24,
-                              color: Colors.grey,
-                            ),
-                            Icon(
-                              Icons.star,
-                              size: 24,
-                              color: Colors.grey,
-                            ),
-                            Icon(
-                              Icons.star,
-                              size: 24,
-                              color: Colors.grey,
-                            ),
-                            Icon(
-                              Icons.star,
-                              size: 24,
-                              color: Colors.grey,
-                            ),
-                            Icon(
-                              Icons.star,
-                              size: 24,
-                              color: Colors.grey,
-                            ),
-                            Container(
-                              child: Text('170 Reviews'),
-                            )
-                          ],
-                        ),
-
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin:
-                    EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 12.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Icon(Icons.check_circle, color: Colors.green,),
-                            Text('PREP'),
-                            Text('25 Min'),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Icon(Icons.timer, color: Colors.green,),
-                            Text('COOK'),
-                            Text('1 Hr'),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Icon(Icons.perm_identity, color: Colors.green,),
-                            Text('FEEDS'),
-                            Text('4-6'),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child:
-              Container(
-                child: OverflowBox(
-                  alignment: Alignment(-1, 0),
-                  minHeight: 0.0,
-                  minWidth: 0.0,
-                  maxWidth: double.infinity,
-                  child: Image
-                    (image: AssetImage('assets/food.png'), fit: BoxFit.cover,),
-                ),
-              ),
-            ),
-          ],
-        );
-      }
-
-      else if(orientation == Orientation.portrait){
-        return Column(
-          children: <Widget>[
-            Expanded (
-              flex: 1,
-              child:
-              Container(
-                child: OverflowBox(
-                  //alignment: Alignment(-1, 0),
-                  //minHeight: 0.0,
-                  //minWidth: 0.0,
-                  //maxWidth: double.infinity,
-                  child: Image
-                    (image: AssetImage('assets/food.png'), fit: BoxFit.cover,),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 24, 0, 0),
-                        child: Center(
-                          child: Text(
-                            "Strawberry Pavolva",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold, ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-                    child: Center(
-                      child: Text(
-                        "pavlova is a meringue-based"
-                            " dessert names after the Russian"
-                            " ballerine Arina Pavlova."
-                            "Pavlova features a crisp crust and "
-                            "soft, light inside, topped with fruit"
-                            "and whipped cream. ",
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-                    child: Wrap(
-                      spacing: 8,
-                      runSpacing: 4,
-                      alignment: WrapAlignment.center,
-                      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(
-                                Icons.star,
-                                size: 24,
-                                color: Colors.grey,
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 24,
-                                color: Colors.grey,
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 24,
-                                color: Colors.grey,
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 24,
-                                color: Colors.grey,
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 24,
-                                color: Colors.grey,
-                              ),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(8, 0, 0, 0),
-                                child: Text('170 Reviews'),
-                              )
-                            ],
-                          ),
-                        ),
-
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin:
-                    EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 12.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Icon(Icons.check_circle, color: Colors.green,),
-                            Text('PREP'),
-                            Text(""),
-                            Text('25 Min'),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Icon(Icons.timer, color: Colors.green,),
-                            Text('COOK'),
-                            Text(""),
-                            Text('1 Hr'),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Icon(Icons.perm_identity, color: Colors.green,),
-                            Text('FEEDS'),
-                            Text(""),
-                            Text('4-6'),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        );
-      }
+  Widget build(BuildContext context) {
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
+      return portrait();
+    } else {
+      return landscape();
     }
-      //body: Container(
-        //decoration: BoxDecoration(
-          //border: Border.all(color: Colors.red, width: 2),
-        //),
-
-      //),
-
-      @override
-      Widget build(BuildContext context) {
-    return Scaffold(
-      body: _layOut());
-
   }
 }
 
+Widget title = Text(
+  'Strawberry Pavlova',
+  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18), textAlign: TextAlign.center,
+);
+Widget description = Text(
+  'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova.'
+      ' It is a meringue dessert with a crisp crust and soft, light inside,'
+      ' usually topped with fruit and whipped cream.',
+  textAlign: TextAlign.center,
+);
 
+Widget landscape() {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: <Widget>[
+      Expanded(
+        flex: 1,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                title,
+                description,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Wrap(
+                      children: <Widget>[
+                        StarIconsRow(),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text('170 reviews'),
+                      ],
+                    ),
+                  ],
+                ),
+                LastSegment()
+              ],
+            ),
+          ),
+        ),
+      ),
+      Expanded(
+          flex: 2,
+          child: FoodImage()),
+    ],
+  );
+}
+
+Widget portrait() {
+  return Column(
+    children: <Widget>[
+      Expanded(
+          flex: 1,
+          child:  FoodImage()),
+      Expanded(
+        flex: 1,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              title,
+              description,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Wrap(
+                    children: <Widget>[
+                      StarIconsRow(),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('170 reviews'),
+                    ],
+                  ),
+                ],
+              ),
+             LastSegment()
+            ],
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+class FoodImage extends StatelessWidget {
+  const FoodImage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: AssetImage('assets/food.png'),
+      fit: BoxFit.fitHeight,
+    );
+  }
+}
+
+class StarIconsRow extends StatelessWidget {
+  const StarIconsRow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Icon starIcon = Icon(
+      Icons.star,
+      size: 16,
+    );
+    return Row(
+      children: <Widget>[
+        starIcon,
+        starIcon,
+        starIcon,
+        starIcon,
+        starIcon,
+      ],
+    );
+  }
+}
+
+class LastSegment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Icon(Icons.check_circle, color: Colors.green,),
+              Text('PREP'),
+              Text(""),
+              Text('25 Min'),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Icon(Icons.timer, color: Colors.green,),
+              Text('COOK'),
+              Text(""),
+              Text('1 Hr'),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Icon(Icons.perm_identity, color: Colors.green,),
+              Text('FEEDS'),
+              Text(""),
+              Text('4-6'),
+            ],
+          ),
+        ],
+
+    );
+  }
+}
 
